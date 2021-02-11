732afaf923e00d34dbe0b139e3a34658a3563cdd
MedChebbi <1mohamed.chebbi@gmail.com>
2021-02-11 14:53:05 +0100

    0.02

--- argus_base/hardware_node/hardware_node.ino
+++ argus_base/hardware_node/hardware_node.ino
@@ -1,25 +1,35 @@
 #include <ros.h>
 #include <geometry_msgs/Twist.h>
 #include <ros/time.h>
+#include "Kinematics.h"
+
+#define MOTOR_MAX_RPM 300        // motor's maximum rpm
+#define WHEEL_DIAMETER 0.065      // robot's wheel diameter expressed in meters
+#define FR_WHEEL_DISTANCE 0.6   // distance between front wheel and rear wheel
+#define LR_WHEEL_DISTANCE 0.333   // distance between left wheel and right wheel
+#define PWM_BITS 8              // microcontroller's PWM pin resolution.
+//motor drive pins
+#define l_speed_pin  5
+#define l_w1         7
+#define l_w2         6
+#define r_w1         9
+#define r_w2         8
+#define r_speed_pin  10
 
 ros::NodeHandle nh;
-
-//motor drive pins
-const int l_speed_pin = 5;
-const int l_w1 = 7;
-const int l_w2 = 6;
-const int r_w1 = 9;
-const int r_w2 = 8;
-const int r_speed_pin = 10;
+Kinematics kinematics(MOTOR_MAX_RPM, WHEEL_DIAMETER, FR_WHEEL_DISTANCE, LR_WHEEL_DISTANCE, PWM_BITS);
 
 double x = 0;
 double y = 0;
 double theta = 0;
-float motor_speed =0;
+float motor_speed = 0;
 
 // cmd_vel variables to be received to drive with
 float demandx;
 float demandz;
+
+int pwmL = 0;
+int pwmR = 0;
 
 // timers for the sub-main loop
 unsigned long currentMillis;
@@ -39,6 +49,7 @@ void setup() {
   nh.initNode();              // init ROS
   nh.subscribe(sub);          // subscribe to cmd_vel
 
+  //Serial.begin(9600);
   //setup the pins for motor control
   pinMode(r_w1,OUTPUT);
   pinMode(r_w2,OUTPUT);
@@ -52,9 +63,29 @@ void setup() {
 void loop() {
   nh.spinOnce();
   moving(demandx,demandz);
-  delay(5);
+  delay(2);
 }
 
+void right_forward(){
+  digitalWrite(r_w1,LOW);
+  digitalWrite(r_w2,HIGH);
+}
+
+void left_forward(){
+  digitalWrite(l_w1,LOW);
+  digitalWrite(l_w2,HIGH);
+}
+
+void right_reverse(){
+  digitalWrite(r_w1,HIGH);
+  digitalWrite(r_w2,LOW);
+}
+
+void left_reverse(){
+  digitalWrite(l_w1,HIGH);
+  digitalWrite(l_w2,LOW);
+}
+/*
 void forward(float x){
   digitalWrite(r_w1,LOW);
   digitalWrite(r_w2,HIGH);
@@ -102,7 +133,7 @@ void rot_left(float z){
 
   analogWrite(r_speed_pin,int(motor_speed));
   analogWrite(l_speed_pin,int(motor_speed));
-}
+}*/
 void sstop(){
   digitalWrite(r_w1,LOW);
   digitalWrite(r_w2,LOW);
@@ -113,9 +144,33 @@ void sstop(){
 }
 
 void moving(float lin, float ang){
-  if (lin>0) forward(lin);
+  Kinematics::output pwm;
+  pwm = kinematics.getPWM(lin, 0, ang);
+ if (pwm.motor1>0) {
+  left_forward();
+  pwmL = constrain(pwm.motor1,0,255);
+  analogWrite(l_speed_pin,pwmL);
+ }
+ else {
+  left_reverse();
+  pwmL = constrain(pwm.motor1,-255,0);
+  pwmL = map(pwmL,0,-255,0,255);
+  analogWrite(l_speed_pin,pwmL);  
+ }
+ if (pwm.motor2>0){
+  right_forward();
+  pwmR = constrain(pwm.motor2,0,255);
+  analogWrite(r_speed_pin,pwmR);
+ }
+ else {
+  right_reverse();
+  pwmR = constrain(pwm.motor2,-255,0);
+  pwmR = map(pwmR,0,-255,0,255);
+  analogWrite(r_speed_pin,pwmR);
+ }
+ /* if (lin>0) forward(lin);
   else if (lin<0) backward(lin);
   else if (ang<0) rot_right(ang);
   else if (ang>0) rot_left(ang);
-  else sstop();
+  else sstop();*/
 }
