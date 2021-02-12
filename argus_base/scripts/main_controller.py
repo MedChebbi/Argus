4176be7b5b0a270c1b0e00c49e9bbee0e7135885
MedChebbi <1mohamed.chebbi@gmail.com>
2021-02-12 13:19:20 +0100

    0.02

--- argus_base/scripts/main_controller.py
+++ argus_base/scripts/main_controller.py
@@ -21,7 +21,7 @@ class Controller(object):
 		self.vel.angular.z = 0
 		self.line_info = LineInfo()
 		self.auto_mode = Bool()
-		self.auto_mode = False
+		self.auto_mode = True
 		self.pid_param = {"KP": 0.05, "KI": 0, "KD": 0}
 		srv = Server(pidParamConfig, self.reconfig)
 		self.Kp, self.Ki, self.Kd = self.pid_param["KP"], self.pid_param["KI"], self.pid_param["KD"]
@@ -30,39 +30,38 @@ class Controller(object):
 		self.teleop_sub = rospy.Subscriber('/teleop/command', Bool, self.teleCallback)
 		self.line_follow_sub = rospy.Subscriber("detected_line_info", LineInfo , self.lineCallback)
 		self.vel_sub = rospy.Subscriber('/teleop/cmd_vel', Twist, self.callback)
-		
+	
+	def run(self):
+		while not rospy.is_shutdown():
+			#self.teleop_sub = rospy.Subscriber('/teleop/command', Bool, self.teleCallback)
+			if self.auto_mode == True:
+				print("automatic")
+				print(self.line_info.error)
+				if self.line_info.detected == True:
+					self.vel.linear.x = 0.4
+					self.vel.angular.z = self.pid(self.line_info.error)
+					print("after pid robot speed: \n", str(self.vel))
+				else:
+					self.vel.linear.x = -0.4
+					print("no line in auto mode \n robot speed: \n", str(self.vel))
+			else:
+				print("manual")
+			self.publish()
+			self.rate.sleep()
+
 	def publish(self):
 		self.pub.publish(self.vel)
 		print("publishing")
 
 	def callback(self, data):
-		if self.auto_mode == False:
-			self.vel = data
-			print("robot speed: \n", str(self.vel))
-		else:
-			pass
-		self.publish()
+		self.vel = data
 
 	def lineCallback(self, data):
 		self.line_info = data
-		if self.auto_mode == True:
-
-			print(self.line_info.error)
-			if self.line_info.detected == True:
-				self.vel.linear.x = 0.4
-				self.vel.angular.z = self.pid(self.line_info.error)
-				print("after pid robot speed: \n", str(self.vel))
-
-			else:
-				self.vel.linear.x = -0.4
-				print("no line in auto mode \n robot speed: \n", str(self.vel))
-		else:
-			pass
-		self.publish()
 
 	def teleCallback(self, data):
 		self.auto_mode = data
-		#print(self.auto_mode)
+		print(self.auto_mode)
 
 	def reconfig(self, config, level):
 		self.pid_param = config
@@ -73,7 +72,6 @@ if __name__=="__main__":
 	rospy.init_node("argus_head_node")
 	controller = Controller()
 	try:
-		rospy.spin()
-		print("done")
+		controller.run()
 	except rospy.ROSInterruptException as e:
 		print(e)
