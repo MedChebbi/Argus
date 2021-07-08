e1b19cb6eab145bdcd1263b5fa01d0e59576178a
Rad-hi <radhisghaier33@gmail.com>
2021-07-08 16:02:08 +0100

    Added a seperate file for the sequence creation

---
+++ robotic_arm/robotic_arm/sequence.h
@@ -0,0 +1,22 @@
+#ifndef __SEQUENCE__
+  #define __SEQUENCE__
+
+    #include "Arduino.h"
+    
+    // Default sequences creation
+    #define SEQUENCE_LEN            12    // Max steps count in each sequence
+    #define SEQ_ID_LEN              5     // Length of the sequence ID
+    #define NUM_SEQEUNCES           2     // Number of default sequences
+
+    // Default sequences container 
+    struct pair{
+      uint8_t x;     // Value in millimeter
+      uint8_t z;  
+    }; // Holds the (x, z) point to go to
+    
+    struct SEQUENCE{
+      char id_ [SEQ_ID_LEN];
+      pair sequence[SEQUENCE_LEN];
+    }; // Holds the sequence of points to target
+    
+#endif
