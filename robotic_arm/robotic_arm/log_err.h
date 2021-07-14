673cc944be3b3d539e9031fa72d13b7ea18707c1
Rad-hi <radhisghaier33@gmail.com>
2021-07-14 10:17:47 +0100

    Created a seperate file for error format/codes definition

---
+++ robotic_arm/robotic_arm/log_err.h
@@ -0,0 +1,17 @@
+#ifndef __LOG_ERR__    
+  #define __LOG_ERR__
+    
+    // Error log queue
+    #define LOG_QUEUE_LEN           6     // How many error messages to hold
+    #define LOG_BUFF_LEN            24    // How long each error msg will be (max length)
+    
+    // LOGGING CODES
+    #define INVALID_LOG             0
+    #define UNKNOWN_LOG             1
+
+    struct LOG_MSG{
+      bool have_log = false;
+      char log_buf[LOG_BUFF_LEN];         // Error reporter buff
+    };
+
+#endif
