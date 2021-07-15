a08b1e4194aae3c702d89f5daf3d50e31d3b6d0b
Rad-hi <radhisghaier33@gmail.com>
2021-07-15 14:47:17 +0100

    Changed the default sequence struct from pair of points to commands

--- robotic_arm/robotic_arm/sequence.h
+++ robotic_arm/robotic_arm/sequence.h
@@ -7,17 +7,28 @@
     #define MAX_SEQUENCE_LEN        12    // Max steps count in each sequence
     #define SEQ_ID_LEN              5     // Length of the sequence ID
     #define NUM_SEQEUNCES           2     // Number of default sequences
+    #define SEQ_CMD_LEN             6 
 
+    /*
     // Default sequences container 
     struct pair{
       uint8_t x;     // Value in centimeter
       uint8_t z;  
     }; // Holds the (x, z) point to go to
-    
+
+    // This holds a sequence of pure points to go to
     struct SEQUENCE{
       char id_ [SEQ_ID_LEN]; // For now, the id is the idx .. might make a hashing func later
       uint8_t len;
       pair sequence[MAX_SEQUENCE_LEN];
     }; // Holds the sequence of points to target
+    */
+
+    // This holds a sequence of commands, which would allow for gripping to happen in the middle of the sequence
+    struct CMD_SEQUENCE{
+      char id_[SEQ_ID_LEN];
+      uint8_t len;
+      char cmd_[MAX_SEQUENCE_LEN][SEQ_CMD_LEN];
+    };
     
 #endif
